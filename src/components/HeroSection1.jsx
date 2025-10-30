72e00fa2f4e18b9d40ccdf8cb871b1436591da79
Gopi Krishna <your-email@example.com>
2025-10-30 16:34:08 +0530

    HeroSection1.jsx

---
+++ src/components/HeroSection1.jsx
@@ -0,0 +1,56 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import backgroundVideo from "../assets/digital-marketing-bg6.mp4";
+
+const HeroSection = () => {
+  return (
+    <section
+      id="home"
+      className="relative min-h-screen w-full flex items-center justify-start overflow-hidden"
+    >
+      {/* 🔹 Background Video */}
+      <div className="absolute inset-0 w-full h-full">
+        <video
+          className="w-full h-full object-cover brightness-auto"
+          src={backgroundVideo}
+          autoPlay
+          loop
+          muted
+          playsInline
+        />
+        {/* Overlay */}
+
+      </div>
+      <div className="absolute inset-0 bg-black/20"></div>
+
+      {/* 🔹 Content */}
+      <div className="relative z-10 w-full max-w-10xl mx-auto px-6 sm:px-10 lg:px-20 py-20 text-center lg:text-left">
+        <h1 className="text-transparent bg-clip-text bg-linear-to-r from-white to-blue-400 text-2xl sm:text-3xl lg:text-5xl font-extrabold mb-4 leading-tight">
+          Welcome to Ardent and Vale Consultants Pvt Ltd
+        </h1>
+
+
+        <p className="text-white text-lg sm:text-3xl font-semibold mb-6">
+          <b>Architecting The Digital Change</b>
+        </p>
+
+        <p className="text-white/90 text-base sm:text-lg leading-relaxed max-w-8xl mb-8">
+          At Ardent and Vale Consultants Pvt Ltd, we leverage the latest technology to
+          propel your business forward to make it scalable, secure, and
+          future-ready. Powered by engineers with a problem-solving mindset, we
+          deliver outcomes that go beyond expectations.
+        </p>
+
+        <Link
+          to="/about"
+          className="inline-block bg-slate-900 hover:bg-slate-600 text-white font-semibold text-lg px-8 py-3 rounded-md shadow-md transition-all duration-300 hover:scale-105"
+        >
+          Read More
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
